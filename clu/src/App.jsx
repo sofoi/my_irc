85e6a462a2c57938e4025b7fd15639e194c5ee08
Mr.robot <sofiane.molle@epitech.eu>
2020-05-16 02:46:09 +0200

    suite

--- clu/src/App.jsx
+++ clu/src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import {React} from "react";
 import ReactDOM from 'react-dom';
 
 import { Link } from 'react-router-dom';
+
 class App extends React.Component {
   render() {
     return(
--- clu/src/index.js
+++ clu/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import { React } from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
-import App, { Register, Login } from './App';
+import App, { Register, Login } from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter , Route, Switch } from 'react-router-dom';
 
@@ -28,4 +28,4 @@ ReactDOM.render((
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
